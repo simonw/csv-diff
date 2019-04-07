34c2d298c77b30ef27d0ddd8f8364acf3ebb2a03
Simon Willison <swillison@gmail.com>
2019-04-06 21:02:11 -0700

    Neater tests using textwrap.dedent

--- tests/test_human_text.py
+++ tests/test_human_text.py
@@ -1,5 +1,7 @@
 from csv_diff import load_csv, compare, human_text
 from .test_csv_diff import ONE, TWO, THREE, FOUR
+
+from textwrap import dedent
 import io
 
 
@@ -7,26 +9,34 @@ def test_row_changed():
     diff = compare(
         load_csv(io.StringIO(ONE), key="id"), load_csv(io.StringIO(TWO), key="id")
     )
-    assert '1 row changed\n\n  Row 1\n    age: "4" => "5"' == human_text(diff)
+    assert dedent("""
+    1 row changed
+
+      Row 1
+        age: "4" => "5"
+    """).strip() == human_text(diff)
 
 
 def test_row_added():
     diff = compare(
         load_csv(io.StringIO(THREE), key="id"), load_csv(io.StringIO(TWO), key="id")
     )
-    assert '1 row added\n\n  {"id": "2", "name": "Pancakes", "age": "2"}' == human_text(
-        diff
-    )
+    assert dedent("""
+    1 row added
+
+      {"id": "2", "name": "Pancakes", "age": "2"}
+    """).strip() == human_text(diff)
 
 
 def test_row_removed():
     diff = compare(
         load_csv(io.StringIO(TWO), key="id"), load_csv(io.StringIO(THREE), key="id")
     )
-    assert (
-        '1 row removed\n\n  {"id": "2", "name": "Pancakes", "age": "2"}'
-        == human_text(diff)
-    )
+    assert dedent("""
+    1 row removed
+
+      {"id": "2", "name": "Pancakes", "age": "2"}
+    """).strip() == human_text(diff)
 
 
 def test_row_changed_and_row_added():
@@ -34,11 +44,15 @@ def test_row_changed_and_row_added():
     diff = compare(
         load_csv(io.StringIO(ONE), key="id"), load_csv(io.StringIO(FOUR), key="id")
     )
-    assert (
-        "1 row changed, 1 row added\n\n"
-        "1 row changed\n\n"
-        "  Row 1\n"
-        '    age: "4" => "5"\n\n'
-        "1 row added\n\n"
-        '  {"id": "3", "name": "Bailey", "age": "1"}'
-    ) == human_text(diff)
+    assert dedent("""
+    1 row changed, 1 row added
+
+    1 row changed
+
+      Row 1
+        age: "4" => "5"
+
+    1 row added
+
+      {"id": "3", "name": "Bailey", "age": "1"}
+    """).strip() == human_text(diff)
